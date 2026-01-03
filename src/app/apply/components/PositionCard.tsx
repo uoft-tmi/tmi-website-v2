7258260f2094742d00de0f49ca23218fed5e9fa4
peterlee42 <76502486+peterlee42@users.noreply.github.com>
2026-01-02 23:03:37 -0500

    refactor: modify position card

--- src/app/apply/components/PositionCard.tsx
+++ src/app/apply/components/PositionCard.tsx
@@ -12,7 +12,7 @@ export function PositionCard({
 }) {
     return (
         <div className="bg-card p-4 rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 justify-between flex flex-col gap-1">
-            <div className="flex items-center justify-between">
+            <div className="flex md:flex-row md:items-center md:justify-between md:gap-1 flex-col gap-4">
                 <div className="flex gap-4 items-center">
                     <div>
                         <p className="flex items-center gap-1 text-status-open text-sm font-medium bg-green-100 px-2 py-1 rounded-full">
@@ -20,7 +20,7 @@ export function PositionCard({
                             <CheckCircleIcon className="stroke-status-open size-4" />
                         </p>
                     </div>
-                    <h3 className="text-xl flex justify-items-start font-semibold tracking-tight text-gray-900">
+                    <h3 className="md:text-xl text-md flex justify-items-start font-semibold tracking-tight text-gray-900">
                         {positionTitle}
                     </h3>
                 </div>
@@ -30,7 +30,7 @@ export function PositionCard({
                         target="_blank"
                         rel="noopener noreferrer"
                     >
-                        <button className=" bg-orange-500 hover:bg-orange-600 text-white font-semibold text-sm rounded-lg py-2 px-4 transition-colors duration-200">
+                        <button className="w-full bg-orange-500 hover:bg-orange-600 text-white font-semibold text-xs rounded-lg py-2 px-4 transition-colors duration-200">
                             Apply
                         </button>
                     </a>
